da85cc96b1725df8b9612d1fcb3f37d005b22aff
murali tolikonda <mtolikonda03@gmail.com>
2024-04-07 16:05:51 +0100

    created new commit to print

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# comments
+
+echo "Ramesh: hello suresh, good morning"
+echo "suresh: hello ramesh,very good morning"
+echo "ramesh: how are you? ramesh"
+echo "suresh: am good ramesh"
