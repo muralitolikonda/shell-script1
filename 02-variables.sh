7c99df2c614c8950258cdf0e660fc6903394e1b3
murali tolikonda <mtolikonda03@gmail.com>
2024-04-07 17:37:47 +0100

    created new commit to print

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,5 +5,5 @@ PERSON2=Ram
 
 echo "$PERSON1: hello $PERSON2, good morning"
 echo "$PERSON2: hello $PERSON1,very good morning"
-echo "r$PERSON1: how are you? $PERSON1"
+echo "$PERSON1: how are you doing? $PERSON1"
 echo "$PERSON2: am good $PERSON1"
