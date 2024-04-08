a0a0bbe350d7df724a2834b2c348b2293f702335
murali tolikonda <mtolikonda03@gmail.com>
2024-04-08 20:18:39 +0100

    saving the script to use with arguments

--- 09-condition1.sh
+++ 09-condition1.sh
@@ -5,6 +5,7 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then 
     echo "ERROR:: Please run the script with the root user"
+    exit 1 # we are stopping the script to run 
 
 else 
     echo "you are root user"
