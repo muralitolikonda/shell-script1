98eebbdce88f108e57b141eacb6ad1eef625ed31
murali tolikonda <mtolikonda03@gmail.com>
2024-04-08 10:56:16 +0100

    saving the script to use with arguments

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hello $PERSON2, good morning"
+echo "$PERSON2: hello $PERSON1,very good morning"
+echo "$PERSON1: how are you doing? $PERSON1"
+echo "$PERSON2: am good $PERSON1"
