8ca539c9efe6231abe39c862bc02de5ca7f007c9
murali tolikonda <mtolikonda03@gmail.com>
2024-04-08 17:42:22 +0100

    saving the script to use with arguments

--- 09-condition.sh
+++ 09-condition.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 if [ $NUMBER -gt 100 ]
- then
- 
-echo "Given number $NUMBER is greater than 100"
+then
+    echo "Given number $NUMBER is greater than 100"
 
 else
-
- echo "Given number $NUMBER is not greater than 100"
-
+    echo "Given number $NUMBER is not greater than 100"
 if
