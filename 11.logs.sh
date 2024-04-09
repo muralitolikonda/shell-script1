fd9fba529b3d7015d7bd50f384442b0d8840bf57
murali tolikonda <mtolikonda03@gmail.com>
2024-04-09 11:41:13 +0100

    saving the script to use with arguments

---
+++ 11.logs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "Script name: $0"
+
+VALIDATE () {
+    if [ $1 -ne 0 ]
+    then 
+        echo "ERROR:: $2 ...failed"
+        exit 1
+    else
+        echo "$2 ...success"
+    fi
+
+}
+
+
+if [ $ID -ne 0 ]
+then 
+    echo "ERROR:: Please run the script with the root user"
+    exit 1 # we are stopping the script to run 
+
+else 
+    echo "you are root user"
+fi
+
+
+yum install mysql -y
+
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
