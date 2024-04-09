adbd43a6ed416cd787af71653eb52955fac3d1f9
murali tolikonda <mtolikonda03@gmail.com>
2024-04-09 15:55:20 +0100

    saving the script to use with arguments

--- 11-logs.sh
+++ 11-logs.sh
@@ -11,10 +11,10 @@ LOGFILE="/tmp/$0-$TIMESTAMP.LOG"
 VALIDATE () {
     if [ $1 -ne 0 ]
     then 
-        echo -e "ERROR:: $2 ...\e[31 failed"
+        echo -e "ERROR:: $2 ...\e[31m failed"
         exit 1
     else
-        echo -e "$2 ...\e[32 success"
+        echo -e "$2 ...\e[32m success"
     fi
 
 }
