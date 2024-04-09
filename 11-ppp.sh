5610823014c56408d933d79effb03c6b9666bf89
murali tolikonda <mtolikonda03@gmail.com>
2024-04-09 14:34:48 +0100

    saving the script to use with arguments

---
+++ 11-ppp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Total:: $SUM "
+
+echo "how many arguments passed:: $#"
+echo "All arguments:: $@"
+echo "script name:: $0"
