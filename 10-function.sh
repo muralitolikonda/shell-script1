b46322c76e6273b29b97f400e415146d19d38f2d
murali tolikonda <mtolikonda03@gmail.com>
2024-04-09 10:13:19 +0100

    saving the script to use with arguments

--- 10-function.sh
+++ 10-function.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+VALIDATE () {
+    if [ $? -ne 0 ]
+    then 
+        echo "ERROR:: Installing mysql failed"
+        exit 1
+    else
+        echo "Installing mysql success"
+    fi
+
+}
+
 ID=$(id -u)
 
 if [ $ID -ne 0 ]
@@ -11,16 +22,6 @@ else
     echo "you are root user"
 fi
 
-VALIDATE () {
-    if [ $? -ne 0 ]
-    then 
-        echo "ERROR:: Installing mysql failed"
-        exit 1
-    else
-        echo "Installing mysql success"
-    fi
-
-}
 
 yum install mysql -y
 
