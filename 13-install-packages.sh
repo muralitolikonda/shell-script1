5e82c446b62a9aaa503254149f36f236caa51914
murali tolikonda <mtolikonda03@gmail.com>
2024-04-10 12:15:32 +0100

    saving the script to use with arguments

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -15,10 +15,10 @@ echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 VALIDATE() {
     if [ $1 -ne 0 ]
     then 
-        echo "$2 ...$R FAILED $N"
+        echo -e "$2 ...$R FAILED $N"
         exit 1
     else
-        echo "$2 ...$G SUCCESS $N"
+        echo -e "$2 ...$G SUCCESS $N"
     fi
 }
 
